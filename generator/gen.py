47193aeb0a319929e65b5d49e68aea76a14fe125
JongMan Koo <jongman@gmail.com>
2011-02-06 00:34:51 -0600

    changed generator. 4 files each w/ 20 test cases.

--- generator/gen.py
+++ generator/gen.py
@@ -3,10 +3,13 @@
 
 from random import random
 
-MAX_N = 50
-for n in xrange(1,MAX_N):
-    fp = open("input%.2d.txt" % n, "w")
-    fp.write("%d\n" % n);
-    for i in xrange(n):
-        fp.write("%.10lf %.10lf\n" % (random() * 1000, random() * 1000))
+names = ["small", "medium", "large", "xlarge"]
+Ns = [10, 15, 20, 25]
+for no, name, n in zip(range(len(Ns)), names, Ns):
+    fp = open("%d.%s.txt" % (no, name), "w")
+    fp.write("20\n")
+    for cc in xrange(20):
+        fp.write("%d\n" % n);
+        for i in xrange(n):
+            fp.write("%.10lf %.10lf\n" % (random() * 1000, random() * 1000))
     fp.close()
